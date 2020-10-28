7788b7991c33d10a02043dba4513f37a8650a939
jccg <=>
2020-10-28 13:52:23 -0300

    se agrega reama cientifica jc

---
+++ ejercicio_individual.py
@@ -0,0 +1,10 @@
+x = 3                   
+f = 3.1415926               
+name = "Python"
+print(x)
+print(f)
+print(name)
+combination = name + " " + name
+print(combination)
+suma = f + f
+print(suma)
