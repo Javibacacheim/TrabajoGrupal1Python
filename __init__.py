093551c999b7d20502eb87a93d339aaf6580028d
Javibacacheim <jibacacheim@gmail.com>
2020-10-31 19:12:35 -0300

    se modifica archivo __init__.py

--- __init__.py
+++ __init__.py
@@ -2,3 +2,21 @@ from .utils import PI
 from .utils import C
 from .calculos import vol_cilindro
 from .calculos import multiplica
+
+#importa de scripts grupo4
+
+from .estadistica import promedio
+from .estadistica import rango
+from .estadistica import media_geo
+from .estadistica import media_armonica
+
+from .calculo_matematico import sumar
+from .calculo_matematico import restar
+from .calculo_matematico import dividir
+from .calculo_matematico import multiplicar
+
+from .calculos_fisicos import calcula_rapidez
+from .calculos_fisicos import calcula_distancia
+from .calculos_fisicos import calcula_tiempo
+from .calculos_fisicos import cel_a_far
+from .calculos_fisicos import far_a_cel
