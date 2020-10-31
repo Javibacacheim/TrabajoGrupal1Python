f78729eadebec3a129fade4e6029978e54e5bdf1
jccg <=>
2020-10-31 16:45:30 -0300

    Se agregan funciones de calculos fisicos

--- calculos_fisicos.py
+++ calculos_fisicos.py
@@ -1,10 +1,33 @@
-x = 3                   
-f = 3.1415926               
-name = "Python"
-print(x)
-print(f)
-print(name)
-combination = name + " " + name
-print(combination)
-suma = f + f
-print(suma)
+#Formula que calcula la rapidez de un objeto en base a la distancia recorrida y el tiempo ocupado
+def calcula_rapidez(distancia, tiempo):
+    rapidez = distancia/tiempo
+    return rapidez
+
+#Formula que calcula la la distancia recorrida de un objeto en base al tiempo utilizado y su velocidad
+def calcula_distancia(tiempo, rapidez):
+    distancia = tiempo*rapidez
+    return distancia
+
+#Formula que calcula el tiempo que tarda un objeto en recorrer cierta distancia
+def calcula_tiempo(distancia, rapidez):
+    tiempo = distancia/rapidez
+    return tiempo
+
+#Formula que convierte de grados Celsius a Farenheit
+def cel_a_far(celsius):
+    resultado = celsius*1.8+32
+    return "Grados Celsius ingresados:",celsius," Grados Farenheit :",resultado
+
+#Formula que convierte de grados Farenheit a Celsius
+def far_a_cel(fare):
+    resultado = (fare-32)/1.8
+    return "Grados Farenheit ingresados:",fare," corresponde en Celsius :",resultado
+
+
+
+if __name__ =="__main__":
+    print(calcula_rapidez(10,40))
+    print(calcula_distancia(60,20))
+    print(calcula_tiempo(150,80))
+    print(cel_a_far(33))
+    print(far_a_cel(110))
